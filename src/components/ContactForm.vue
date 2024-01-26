7b1823d0ea089bac5ad3a19909acdf4998601865
Majtthieu <matthieu.jankowiak@yahoo.fr>
2024-01-26 13:22:27 +0100

    contact form avec emailjs et style boutons

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,42 +1,47 @@
 <template>
     <div class="form-container">
-    <h3>Contactez-moi</h3>
-    <form @submit="submitForm">
-        <label for="name">Nom :</label>
-        <input type="text" id="name" v-model="name" required>
-        
-        <label for="email">Email :</label>
-        <input type="email" id="email" v-model="email" required>
-        
-        <label for="message">Message :</label>
-        <textarea id="message" v-model="message" required></textarea>
-        
-        <button type="submit">Envoyer</button>
-    </form>
-    <!-- <div v-if="submitted" class="">
-<p class="">Merci ! À bientôt</p>
-</div> -->
-</div>
+        <h3>Contactez-moi</h3>
+        <form ref="form" @submit.prevent="sendEmail">
+            <label>Nom</label>
+            <input class="form-input" type="text" name="from_name" placeholder="Entrez votre nom svp" required>
+            <label>Email</label>
+            <input class="form-input" type="email" name="reply_to" placeholder="Entrez votre email svp" required>
+            <label>Message</label>
+            <textarea class="form-message" name="message" required style="resize: none;"></textarea>
+            <button class="button_global button_form" type="submit">Envoyer</button>
+        </form>
+        <p v-if="submitted">Votre message a bien été envoyé. À bientôt</p>
+    </div>
 </template>
 
 <script>
+import emailjs from '@emailjs/browser';
 export default {
-    data() {
-        return {
-            name: '',
-            email: '',
-            message: ''
-        };
+  data() {
+    return {
+      submitted: false,
+    };
+  },
+
+  methods: {
+    sendEmail() {
+      emailjs.sendForm("service_t16hr35", "template_x12a01n", this.$refs.form, "_Aybmf8YJu73jv86J")
+        .then((result) => {
+          console.log('SUCCESS!', result.text);
+          this.submitted = true;
+          this.clearForm();
+        }, (error) => {
+          console.log('FAILED...', error.text);
+          this.submitted = false;
+        });
     },
-    methods: {
-        submitForm() {
-            const subject = 'Nouveau message de ' + this.name;
-            const body = 'Email : ' + this.email + '\n\n' + 'Message : ' + this.message;
-            const mailtoLink = 'mailto:ricpau51@gmail.com?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
-            window.location.href = mailtoLink;
-        }
+    clearForm() {
+      this.$refs.form.reset();
     }
+  }
 };
 </script>
 
-<style scoped>@import "./ContactForm.sass";</style>
+<style scoped>
+@import "./ContactForm.sass";
+</style>
